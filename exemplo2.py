e53fb81dc4d1e833f3da9f77976bd23d73c9a059
DD <denilsonsilvapereirafilho737@gmail.com>
2024-09-03 09:46:49 -0300

    Calculando média com python

--- exemplo2.py
+++ exemplo2.py
@@ -3,3 +3,12 @@ end = input("informe seu endereço: ")
 idade = input("Informe sua idade: ")
 
 print(nome, end, idade)
+
+#1ª forma de concentração
+print("\Olá ",nome, "seu endereço é",end," sua idade é", idade)
+
+#2ª forma de concatenação
+print("\Olá {} seu endereço é {} e sua idade é {}".format(nome, end, idade))
+
+#3ª forma de concatenação - f string
+print(f"Bem vindo {nome}, você mora na {end} e tem {idade} anos")
---
+++ exemplo3.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Informe sua nota 1ª nota: "))
+nota2 = float(input("Informe sua nota 2ª nota: "))
+nota3 = float(input("Informe sua nota 3ª nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+print(f"Sua média é {media:.2f}")
+
